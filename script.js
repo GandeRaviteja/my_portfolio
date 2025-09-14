a6bd11fa9963a8a624993c724413c027b69dab61
GandeRaviteja <2310040080@klh.edu.in>
2025-09-14 21:23:20 +0530

    my portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+/* script.js */
+/* (Optional) Smooth scroll behavior */
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
